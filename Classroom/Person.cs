a58d6bcd66368e46321d3af6099c053028575bc2
Missguiding <lindsey141080@hotmail.com>
2023-04-30 16:59:23 +0200

    Add project files.

---
+++ Classroom/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Classroom
+{
+    class Person
+    {
+        private DateTime _dtmDateOfBirth; //field
+        private int _intAgeInYears = 0;
+        private double _dblAgeInDays = 0D;
+
+        private string FirstName { get; set; } // automatic property, private, class access only
+        private string LastName { get; set; } // automatic property, private, class access only
+        public DateTime BirthDate { get; set; } // automatic property
+
+        public Person(string strFirstName, string strLastName, DateTime dtmBirthDate) // constructor with 3 parameters
+        {
+            FirstName = strFirstName;
+            LastName = strLastName;
+            BirthDate = dtmBirthDate;
+        }
+
+        public string FullName // dynamic property - give back full name
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+
+        public int AgeInYears // dynamic property
+        {
+            get
+            {
+                if ((DateTime.Now.Month < _dtmDateOfBirth.Month) || (_dtmDateOfBirth.Month == DateTime.Now.Month && DateTime.Now.Day < _dtmDateOfBirth.Day))
+                {
+                    _intAgeInYears = DateTime.Now.Year - BirthDate.Year - 1;
+                }
+                else
+                {
+                    _intAgeInYears = DateTime.Now.Year - BirthDate.Year;
+                }
+
+                return _intAgeInYears;
+            }
+        }
+
+        public double AgeInDays // dynamic property
+        {
+            get
+            {
+                _dblAgeInDays = (DateTime.Now - BirthDate).TotalDays;
+
+                return _dblAgeInDays;
+            }
+        }
+    }
+}
---
+++ Classroom/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classroom
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Test based on input constructor:");
+            Person tina = new Person("Tina", "De Vrieuw", new DateTime(1990, 06, 29));
+            Person gilbert = new Person("Gilbert", "Vanghenechten", new DateTime(1959, 02, 28));
+
+            Console.WriteLine(tina.FullName + " " + tina.BirthDate);
+            Console.WriteLine(gilbert.FullName + " " + gilbert.BirthDate + "\n");
+
+            Console.WriteLine("Test Age in years:");
+            Console.WriteLine(gilbert.AgeInYears);
+            Console.WriteLine(tina.AgeInYears + "\n");
+
+            Console.WriteLine("Test Age in days:");
+            Console.WriteLine(Convert.ToInt32(tina.AgeInDays));
+            Console.WriteLine(Convert.ToInt32(gilbert.AgeInDays));
+
+            tina.BirthDate = new DateTime(2023, 04, 12);
+            gilbert.BirthDate = new DateTime(1939, 06, 28);
+
+            Console.WriteLine("\n" + "Test age in years, new input day, month, year:");
+            Console.WriteLine(gilbert.AgeInYears);
+            Console.WriteLine(tina.AgeInYears + "\n");
+
+            Console.WriteLine("Test Age in days:");
+            Console.WriteLine(Convert.ToInt32(tina.AgeInDays));
+            Console.WriteLine(Convert.ToInt32(gilbert.AgeInDays));
+                        
+            Console.ReadLine();
+        }
+    }
+}
