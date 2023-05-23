91e182fbbdccbb022676d656114d095d2ed66b7a
dontdestroy <vincent@copypaste.be>
2023-05-23 08:20:39 +0200

    Remarks on 14.01

--- 14.01 - TryoutSomeStuff - Tutorial/CopyPasteTutorial.cs
+++ 14.01 - TryoutSomeStuff - Tutorial/CopyPasteTutorial.cs
@@ -13,6 +13,7 @@ namespace _14._01___TryoutSomeStuff___Tutorial
 
         public override void SetTutorial(int _intIDTutorial, string _strDescriptionTutorial) // method, 2 parameters with signature abstract class
         {
+            // Do not use "_" for normal variables
             KeyTutorial = _intIDTutorial; 
             DescriptionTutorial = _strDescriptionTutorial;
         }
