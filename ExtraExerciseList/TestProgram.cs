09232e4dee83db23ccc877f83c5c7e4d28bfa987
dontdestroy <vincent@copypaste.be>
2023-05-29 11:20:17 +0200

    Extra Exercise List evaluatie

--- ExtraExerciseList/TestProgram.cs
+++ ExtraExerciseList/TestProgram.cs
@@ -2,29 +2,30 @@
 
 namespace ExtraExerciseList
 {
-    public class TestProgram
+  public class TestProgram
+  {
+    static void Main()
     {
-        static void Main()
-        {
 
-            Person lindsay = new Person("Lindsay", "Legrande");
-            Person gilles = new Person("Gilles", "Bonte");
-            Person mieke = new Person("Mieke", "Vandewalle");
-            Person lemmy = new Person("Lemmy", "de Terminator");
+      Person lindsay = new Person("Lindsay", "Legrande");
+      Person gilles = new Person("Gilles", "Bonte");
+      Person mieke = new Person("Mieke", "Vandewalle");
+      Person lemmy = new Person("Lemmy", "de Terminator");
 
-            Cursus csharp = new Cursus("C#");
+      Cursus csharp = new Cursus("C#");
 
-            csharp.Add(mieke);
-            csharp.Add(lindsay);
-            csharp.Add(gilles);
-            csharp.Add(lemmy);
+      csharp.Add(mieke);
+      csharp.Add(lindsay);
+      csharp.Add(gilles);
+      csharp.Add(lemmy);
 
-            Console.WriteLine("Person {0} {1}", csharp[2].FirstName, csharp[2].LastName);
-            Console.WriteLine("Person {0} {1}", csharp[3].FirstName , csharp[0].LastName);
-            Console.WriteLine("Person {0}", csharp["Lindsay"].FirstName);
-            Console.WriteLine("Person {0}", csharp["Lemmy"].LastName);
-            Console.ReadLine();
-        }
+      Console.WriteLine("Person {0} {1}", csharp[2].FirstName, csharp[2].LastName);
+      Console.WriteLine("Person {0} {1}", csharp[3].FirstName, csharp[0].LastName);
+      Console.WriteLine("Person {0}", csharp["Lindsay"].FirstName);
+      Console.WriteLine("Person {0}", csharp["Lemmy"].LastName);
+      Console.WriteLine("Person {0}", csharp["Prosper"].LastName); // VVDW Extra test
+      Console.ReadLine();
     }
+  }
 }
 
